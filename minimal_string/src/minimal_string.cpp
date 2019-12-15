74a393e9b2716691edc4e0ef645627c70aca5d59
Vadym Holovin <holyvad@gmail.com>
2019-12-15 17:20:55 +0200

    Fixed bugs in the minimal_string

--- minimal_string/src/minimal_string.cpp
+++ minimal_string/src/minimal_string.cpp
@@ -5,17 +5,13 @@ using namespace std;
 int main() {
 	string a, b, c;
 	string min;
-
 	cin >> a >> b >> c;
-	if (a < b) {
-		min = a;
-	} else {
-		min = b;
-	}
 
-	if (c < min) {
+	min = (a <= b) ? a : b;
+	if (c <= min) {
 		min = c;
 	}
+
 	cout << min;
 	return 0;
 }
